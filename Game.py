f923370922f2ca06c64816062f0db0a3f540a758
Hofman Jan <92031623+hofmic92@users.noreply.github.com>
2023-02-21 15:21:05 +0100

    Add files via upload

--- Game.py
+++ Game.py
@@ -1,15 +1,44 @@
 import pygame
-import sys
+from sys import exit
 
 pygame.init()
 
 width = 400
 height = 800
 
+icone = pygame.image.load('Images/Icone.png')
 screen = pygame.display.set_mode((width, height))
 
+pygame.display.set_caption("Flappy Bird")
+pygame.display.set_icon(icone)
+
+clock = pygame.time.Clock()
+
+#Zde jsou obrazky
+bc = pygame.image.load('Images/Background.png')
+back = pygame.transform.scale(bc, (400, 800))
+g = pygame.image.load('Images/Ground.png')
+ground = pygame.transform.scale(g, (400, 200))
+
+
+
+
+#Funkce na načtení obrázků
+def imageLoader():
+    screen.blit(back, (0, 0))
+    screen.blit(ground, (0, 700))
+
+
+
+
+#Tady je True loop, který se stará o běh hlvaního okna
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            sys.exit()
-        pygame.display.flip
+            pygame.quit()
+            exit()
+            
+    imageLoader()
+    pygame.display.update()
+    clock.tick(15)
+    

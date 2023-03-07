3e499c8263484fa5bf6d56ded9f9ed3e181bc296
Hofman Jan <92031623+hofmic92@users.noreply.github.com>
2023-03-07 15:16:50 +0100

    Add files via upload

--- Game.py
+++ Game.py
@@ -1,5 +1,11 @@
 import pygame
+import random
 from sys import exit
+
+
+pipeG_y_pos = random.randint(18, 65)*10
+pipeD_y_pos = pipeG_y_pos - 920 
+
 
 pygame.init()
 
@@ -17,16 +23,22 @@ clock = pygame.time.Clock()
 
 #Zde jsou obrazky
 bc = pygame.image.load('Images/Background.png')
-back = pygame.transform.scale(bc, (1000, 800))
+back = pygame.transform.scale(bc, (500, 800))
 back_x_pos = 0
 
 g = pygame.image.load('Images/Ground.png')
 ground = pygame.transform.scale(g, (1000, 200))
 ground_x_pos = 0
 
-pG = pygame.image.load('Images/PipeG3.png')
-pipeG = pygame.transform.scale(pG, (50, 400))
+pG = pygame.image.load('Images/PipeG.png')
+pipeG = pygame.transform.scale(pG, (60, 800))
 pipeG_x_pos = 480
+#pipeG_y_pos = 400
+
+pD = pygame.image.load('Images/PipeD.png')
+pipeD = pygame.transform.scale(pD, (60, 800))
+pipeD_x_pos = 480
+#pipeD_y_pos = 0
 
 
 
@@ -34,7 +46,8 @@ pipeG_x_pos = 480
 #Funkce na načtení obrázků
 def imageLoader():
     screen.blit(back, (back_x_pos, 0))
-    screen.blit(pipeG, (pipeG_x_pos, 400))
+    screen.blit(pipeG, (pipeG_x_pos, pipeG_y_pos))
+    screen.blit(pipeD, (pipeD_x_pos, pipeD_y_pos))
     screen.blit(ground, (ground_x_pos, 700))
 
 
@@ -49,15 +62,18 @@ while True:
 
     if (pipeG_x_pos < -50):
         pipeG_x_pos = 500
+        pipeD_x_pos = 500
+        pipeG_y_pos = random.randint(18, 65)*10
+        pipeD_y_pos = pipeG_y_pos - 920
 
-    if (back_x_pos < -500):
-        back_x_pos = 0
+
     if (ground_x_pos < -500):
         ground_x_pos = 0
-
-    back_x_pos -= 1
-    pipeG_x_pos -= 3
-    ground_x_pos -= 3
+        
+        
+    pipeG_x_pos -= 5
+    pipeD_x_pos -= 5
+    ground_x_pos -= 5
             
     imageLoader()
     pygame.display.update()
